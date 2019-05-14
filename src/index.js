d4131202be0ebab46bd8d0bbf9ed4f184891640d
smrbts <samueljustinroberts@gmail.com>
2019-05-14 15:00:44 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+let list = document.querySelector("ul#tasks")
+let button = document.querySelector("input[type=submit]")
+let textField = document.getElementById("new-task-description")
+button.addEventListener("click", function(e){
+  e.preventDefault()
+  let description = textField.value
+  let myList= document.createElement("li")
+  myList.innerText = description
+  list.appendChild(myList)
+  console.log(`${description}`)
+
+  textField.value = ""
+
+});
+
+
+
+
+
+
+
 });

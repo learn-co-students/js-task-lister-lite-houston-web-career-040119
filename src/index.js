f47cbb2c59d85e66c21ab1aa90cfe458389886b2
PYN <yxp7531540@gmail.com>
2019-05-14 14:33:27 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let textField = document.querySelector('#new-task-description')
+  let form = document.querySelector('#create-task-form')
+  let button = document.querySelectorAll('#create-task-form input')[1]
+  let todoList = document.querySelector('#list ul')
+  button.addEventListener('click', function(e) {
+    e.preventDefault()
+    let newTodo = document.createElement('li')
+    newTodo.innerText = `${textField.value}`
+    let deleteButton = document.createElement('button')
+    deleteButton.innerText = 'X'
+    newTodo.append(deleteButton)
+    deleteButton.addEventListener('click', function(e){
+      e.preventDefault()
+      for (const li of todoList.children) {
+        if (li.children[0] == e.target) {
+          todoList.removeChild(li)
+        }
+      }
+    })
+    form.reset()
+    todoList.append(newTodo)
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
